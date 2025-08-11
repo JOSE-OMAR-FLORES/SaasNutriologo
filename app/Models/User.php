947b76b7cb460dd54102ef5efcb91a7fd8311ff2
JOSE-OMAR-FLORES <153056143+JOSE-OMAR-FLORES@users.noreply.github.com>
2025-08-11 10:13:54 -0600

    prueba gg

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
     ];
 
     public function pacientes(){
-        return $this->hasMany(Paciente::class, 'nutriologo_id');
+        return $this->hasMany(Paciente::class);
     }
 
     public function citas()
